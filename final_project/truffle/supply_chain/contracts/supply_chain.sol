7ad49e310c99a72e8fb65917fa964743c7f1c813
aakash <Aakash.Kumar@colorado.edu>
2019-04-27 16:47:38 -0600

    working replace prices and add item

--- final_project/truffle/supply_chain/contracts/supply_chain.sol
+++ final_project/truffle/supply_chain/contracts/supply_chain.sol
@@ -18,9 +18,10 @@ contract Inventory {
         PriceStruct[] prices; // Probably don't need to specify "stoarge" type. Seems implied.
     }
 
+    /**
+    Put this back as it was failing a test, will remove this and the test case later
+    */
     PriceStruct public temporary_price_struct_for_testing;
-    PriceStruct[] public price_test;
-
     function use_to_test_passing_struct_from_web3(PriceStruct memory ps) public {
         temporary_price_struct_for_testing = ps;
     }
@@ -54,6 +55,7 @@ contract Inventory {
         items[itemId].quantityAvailable -= decrement;
     }
 
+
     function replacePrices(uint itemId, PriceStruct[] memory priceArray) public {
         // Direct copy not supported: https://github.com/ethereum/solidity/issues/3446
         //items[itemId].prices = priceArray;
@@ -66,9 +68,15 @@ contract Inventory {
         // Not sure if memory is better than storage. Should just be a reference to storage
         PriceStruct[] storage prices = items[itemId].prices; // reference
 
+        /**
+        World have been nice to have helper function to do
+        this here and in additem, but function parametrs have to be
+        memeory and memory items don't have the push.
+        There maybe a way to get around, to figure out later
+         */
         for (uint i = 0; i < prices.length; i++)
         {
-            // Todo, focus on addItem first
+            items[itemId].prices.push(priceArray[i]);
         }
     }
 
@@ -79,55 +87,14 @@ contract Inventory {
     }
 
     function addItem(uint quantity, PriceStruct[] memory priceArray) public returns (uint) {
-        /*
-        Attempt 1:
-        One-liner.
-        Compile error.
-        Automatic "memory" to "storage" copy is not supported
-        Must manually copy over priceArray
-        */
 
-        items[numItems].quantityAvailable = quantity;//Item({quantityAvailable: quantity, dummy: 5, prices: priceArray});
+        items[numItems].quantityAvailable = quantity;
 
         for (uint i = 0; i < priceArray.length; i++) {
             items[numItems].prices.push(priceArray[i]);
         }
+
         numItems++;
-        /*
-        Attempt 2:
-        Create "memory" reference type.
-        All of these lines compile.
-        Testing shows that quantityAvailable is not written to storage.
-        */
-        /*
-        Item memory newItem = items[numItems++];
-        newItem.quantityAvailable = quantity;
-        newItem.prices = priceArray;
-        */
-
-        /*
-        Attempt 3:
-        Create "storage" reference type.
-        Suspect that storage ref required for data to stick.
-        All of these lines except for the last one involving "prices" array compile.
-        */
-        /*
-        */
-        // Item storage newItem = items[numItems++];
-        // newItem.quantityAvailable = quantity;
-        // // newItem.prices = priceArray;
-        // for (uint i = 0; i < priceArray.length; i++)
-        // {
-        //     newItem.prices.push(priceArray[i]);// Todo, focus on addItem first
-        // }
-        // // Todo - expand "prices" array copy
-
-        // return numItems - 1; // Item ID
-        /*
-        Having trouble retrieving item ID in javascript tests.
-        Seems that values are only returned from pure/view functions.
-        Workaround is to just use get<Next/Previous>ItemId() view functions.
-        */
     }
 
     /*
